5d47e666898f2a7152f0b681f6ab7076d82d42ca
Janith Maduranga <59273264+janith27@users.noreply.github.com>
2022-11-06 17:01:12 +0530

    footer completed

--- user/smartagri/src/pages/HomePage.jsx
+++ user/smartagri/src/pages/HomePage.jsx
@@ -3,15 +3,13 @@ import React, { Component, Fragment } from 'react'
 // import Categories from '../components/home/Categories'
 
 import Supply from '../components/home/Supply'
-
 import HomeNavMenuDesktop from '../components/home/HomeNavMenuDesktop'
 import HomeSlider from '../components/home/HomeSlider'
-
 import ReJurnels from '../components/home/ReJurnels'
 import About from '../components/home/About'
 import FooterDesktop from '../components/common/FooterDesktop'
 import HomeNevMobile from '../components/home/HomeNevMobile'
-import HomeTop from '../components/home/HomeTop'
+import FooterMobile from '../components/common/FooterMobile'
 
 export class HomePage extends Component {
   render() {
@@ -36,9 +34,16 @@ export class HomePage extends Component {
         {/* <FeatureProducts /> */}
         <Supply />
         <About />
-       
+        <div className="Desktop">
+          <FooterDesktop />
+          
+        </div>
+
+        <div className="Mobile">
+          <FooterMobile />
+        </div>
         {/* <Categories /> */}
-        <FooterDesktop />
+        
         
       </Fragment>
     )
