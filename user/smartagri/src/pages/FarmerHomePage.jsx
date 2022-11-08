134d67ea6578ff74bd4ba4042418689d08f3bdef
J.M Madushika Sewwandi <jmmsewwandi97@gmail.com>
2022-11-08 11:14:37 +0530

    Farmer Home initialize

---
+++ user/smartagri/src/pages/FarmerHomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FarmerHomePage extends Component {
+  render() {
+    return (
+      <div>FarmerHomePage</div>
+    )
+  }
+}
+
+export default FarmerHomePage
