e8506c9ec12cad28789817f65662ccee86aba656
Janith Maduranga <janithmaduranga27@gmail.com>
2023-01-26 17:53:21 +0530

    resolve bugs v 0.1

--- user/smartagri/src/components/instructor/SearchItem.jsx
+++ user/smartagri/src/components/instructor/SearchItem.jsx
@@ -1,4 +1,4 @@
-import './SearchItem.css'
+// import './SearchItem.css'
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import Dropdown from 'react-bootstrap/Dropdown';
