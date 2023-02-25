f31f5a4735fb30e9a8b5c3810615e34075505736
Janith Maduranga <janithmaduranga27@gmail.com>
2023-02-26 02:53:45 +0530

    update V1.1

--- user/smartagri/src/components/home/Supply.jsx
+++ user/smartagri/src/components/home/Supply.jsx
@@ -4,7 +4,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
-import ph1 from "../../assest/images/supply/urea.jpg";
 import AppURL from "../../api/AppURL";
 import axios from "axios";
 
